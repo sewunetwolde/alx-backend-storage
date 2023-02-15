c8efed56764ea31d3b5a78760626bdb99c628fc9
sewunetwolde <nekodmos@gmail.com>
2023-02-15 15:14:24 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,54 +1,22 @@
 #!/usr/bin/env python3
-"""
-Module web
-"""
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
 import redis
 import requests
-from typing import Callable
 
 
-_redis = redis.Redis()
+redi = redis.Redis()
+count = 0
 
 
-def count_access(func: Callable) -> Callable:
-    """
-    Counts the number of times
-    a url was accessed
-    """
-    def wrapper(*args, **kwargs):
-        """wrapper"""
-        key = "count:{}".format(args[0])
-        _redis.incr(key)
-        return func(*args, **kwargs)
-    return wrapper
-
-
-def get_cache(func: Callable) -> Callable:
-    """
-    Counts the number of times
-    a url was accessed
-    """
-    def wrapper(*args, **kwargs):
-        """wrapper"""
-        key = "result:{}".format(args[0])
-        if _redis.exists(key):
-            data = _redis.get(key)
-            return data.decode('utf-8')
-        return func(*args, **kwargs)
-    return wrapper
-
-
-@get_cache
-@count_access
 def get_page(url: str) -> str:
-    """
-    Obtains the HTML content of a
-    particular URL and returns it
-    """
-    key = "result:{}".format(url)
-
+    """ Impelements core function of get page which is accessed."""
+    redi.set(f"cached:{url}", count)
     response = requests.get(url)
-
-    _redis.set(key, response.text, ex=10)
-
+    redi.incr(f"count:{url}")
+    redi.setex(f"cached:{url}", 10, redi.get(f"cached:{url}"))
     return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
